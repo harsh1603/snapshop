8c8dffc2c7d8470df007808ff27192b78d03b74c
unknown <harshprit512@gmail.com>
2026-05-03 15:55:31 +0530

    updated the database

--- backend/sql/users.sql
+++ backend/sql/users.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 01, 2026 at 05:13 PM
+-- Generation Time: May 03, 2026 at 12:23 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.0.30
 
@@ -34,9 +34,17 @@ CREATE TABLE `users` (
   `email_address` varchar(200) DEFAULT NULL,
   `password` varchar(200) DEFAULT NULL,
   `role` int(11) DEFAULT NULL,
-  `modified_date` datetime NOT NULL DEFAULT current_timestamp(),
-  `mobile_number` int(11) DEFAULT NULL
+  `updatedAt` datetime NOT NULL DEFAULT current_timestamp(),
+  `mobile_number` int(11) DEFAULT NULL,
+  `createdAt` timestamp NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `company_name`, `email_address`, `password`, `role`, `updatedAt`, `mobile_number`, `createdAt`) VALUES
+(1, 'harsh', 'test', 'test@gmail.com', '$2y$10$G0u0awwHCncv6UYQMU6AleYwOHf2UEhX8RI4XwoCECRHCtiZtwD9i', 1, '2026-05-02 22:09:54', 0, '2026-05-03 04:47:16');
 
 --
 -- Indexes for dumped tables
@@ -56,7 +64,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
